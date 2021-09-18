188cc28fb7671b96941b9953da74d55512a3ace8
kramire <kramire@users.noreply.github.com>
2021-09-18 22:38:54 +0200

    feat: hero triangles and initials

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ const Header = () => {
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [showMain, setShowMain] = useState(false);
 
   useEffect(() => {
     if (window.innerWidth > 769) {
@@ -21,32 +22,45 @@ const Header = () => {
     }
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowMain(true);
+    }, 1500);
+  }, []);
+
   return (
     <section id="hero" className="jumbotron">
       <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || `Hi there, I'm`}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
-            {subtitle || "I'm the Unknown Developer."}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <p className="hero-location">
-            <i className="fa fa-map-marker" />
-            <span>{location || 'Somewhere'}</span>
-          </p>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
+        <div className="hero-left-triangle" />
+        <div className="hero-initials">KR</div>
+        {showMain && (
+          <>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={200} distance="30px">
+              <h1 className="hero-title">
+                {title || `Hi there, I'm`}{' '}
+                <span className="text-color-main">{name || 'Your Name'}</span>
+                <br />
+                {subtitle || "I'm the Unknown Developer."}
+              </h1>
+            </Fade>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={200} distance="30px">
+              <p className="hero-location">
+                <i className="fa fa-map-marker" />
+                <span>{location || 'Somewhere'}</span>
+              </p>
+            </Fade>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+              <p className="hero-cta">
+                <span className="cta-btn cta-btn--hero">
+                  <Link to="about" smooth duration={1000}>
+                    {cta || 'Know more'}
+                  </Link>
+                </span>
+              </p>
+            </Fade>
+          </>
+        )}
+        <div className="hero-right-triangle" />
       </Container>
     </section>
   );
